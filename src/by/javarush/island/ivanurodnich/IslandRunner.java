9b49581a4db283cdb5c0290b0bc1070a9a21c9c7
IvanUrodnich <greenklever.u@gmail.com>
2022-07-04 13:25:55 +0300

    созданы первоначальные классы

--- src/by/javarush/island/ivanurodnich/IslandRunner.java
+++ src/by/javarush/island/ivanurodnich/IslandRunner.java
@@ -2,6 +2,6 @@ package by.javarush.island.ivanurodnich;
 
 public class IslandRunner {
     public static void main(String[] args) {
-        System.out.println(123);
+        System.out.println(1);
     }
 }
