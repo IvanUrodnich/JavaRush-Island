e166ee9b0c930a774340514db6a2eb9ea3d9fc0d
IvanUrodnich <greenklever.u@gmail.com>
2022-07-04 13:45:56 +0300

    созданы классы травоядных

---
+++ src/by/javarush/island/ivanurodnich/animal/herbivorous/Horse.java
@@ -0,0 +1,4 @@
+package by.javarush.island.ivanurodnich.animal.herbivorous;
+
+public class Horse extends Herbivorous{
+}
---
+++ src/by/javarush/island/ivanurodnich/animal/herbivorous/Sheep.java
@@ -0,0 +1,4 @@
+package by.javarush.island.ivanurodnich.animal.herbivorous;
+
+public class Sheep extends Herbivorous{
+}
